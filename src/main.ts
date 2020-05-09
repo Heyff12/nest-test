1ff50d2770677cf1e2504b6cfddea0efe1c52263
Heyff12 <645068564@qq.com>
2020-05-09 22:52:18 +0800

    add validate

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 import { logger } from './common/middleware/logger.func.middleware';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter'
@@ -6,7 +8,8 @@ import { HttpExceptionFilter } from './common/exception/http-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
