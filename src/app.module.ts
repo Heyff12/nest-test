97fad3cbea443d89e3edde7be4252a166ad8e827
Heyff12 <645068564@qq.com>
2020-05-11 14:50:40 +0800

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from './configModule/config.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { GraphqlOptions } from './graphql.options'
